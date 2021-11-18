5569ad6e561291e7e30035ea827d60caffeaa9fc
Your Name <you@example.com>
2021-11-18 17:14:42 +0100

    demo

--- src/test/java/DataBaseTestRunner.java
+++ src/test/java/DataBaseTestRunner.java
@@ -1,34 +1,34 @@
 
+
+import opencart.BaseClass;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-
 import java.sql.*;
-import java.util.Properties;
+import java.sql.DriverManager;
 
-public class DataBaseTestRunner {
+public class DataBaseTestRunner extends BaseClass {
     Connection connection=null;
-    Properties properties=new Properties();
     Statement statement=null;
     ResultSet resultSet;
     @BeforeClass
-    public void connectDataBase() throws SQLException {
+    void connectDataBase() throws SQLException {
 
-      connection= DriverManager.getConnection(properties.getProperty("dataBaseUrl"),properties.getProperty("dataBaseUser"),properties.getProperty(""));
+      connection= DriverManager.getConnection
+              (properties.getProperty("dataBaseUrl"),properties.getProperty("dataBaseUser"),properties.getProperty("dataPassword"));
     }
     @Test
     void TC1() throws SQLException {
 
             statement=connection.createStatement();
-            resultSet=statement.executeQuery("select * from oc_customer where email='IuNqB@faSYw.com';");
+            resultSet=statement.executeQuery("select * from oc_customer where email='IuNqB@faSYw.com'");
             resultSet.next();
             Assert.assertEquals(resultSet.getString("email"),"IuNqB@faSYw.com");
 
     }
     @AfterClass
-    public void disconnectDataBase() throws SQLException {
+    void disconnectDataBase() throws SQLException {
 
             connection.close();
 
