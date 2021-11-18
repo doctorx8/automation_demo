6201915d652d85b5d88173b191015c8abd68fda5
Your Name <you@example.com>
2021-11-18 21:57:14 +0100

    demo

--- src/test/java/restApi.java
+++ src/test/java/restApi.java
@@ -5,15 +5,9 @@ import static io.restassured.RestAssured.given;
 public class restApi {
     Response response;
     @Test
-    public void getMapLocation(){
-    response=given().log().all().get("http://api.positionstack.com/v1/forward?access_key=e31effaa611e43656fed10673266a5dc&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC");
-    response.then().assertThat().statusCode(200);
-        System.out.println( response.getBody().prettyPrint());
-
-
-
+    public void getMapLocation() {
+        response = given().log().all().get("http://api.positionstack.com/v1/forward?access_key=e31effaa611e43656fed10673266a5dc&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC");
+        response.then().assertThat().statusCode(200);
+        System.out.println(response.getBody().prettyPrint());
     }
-
-
-
 }
