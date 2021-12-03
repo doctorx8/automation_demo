7a41837b4278b7fbf8c83b4fef85e41a3c509e10
Your Name <you@example.com>
2021-12-03 23:29:24 +0100

    demo

--- src/main/java/opencart/frontend/HomePage.java
+++ src/main/java/opencart/frontend/HomePage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.PageFactory;
 public class HomePage extends BaseClass {
         @FindBy(xpath = "//span[text()='My Account']")
         WebElement myAccountLink;
-        @FindBy(css = "//a[text()='Register']")
+        @FindBy(xpath = "//a[text()='Register']")
         WebElement registerLink;
     public HomePage() {
         PageFactory.initElements(driver,this);
