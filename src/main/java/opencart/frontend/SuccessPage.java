ae81ee2c87e6f3b61aea8a35faa5c44785c4705c
Your Name <you@example.com>
2022-02-06 18:08:26 +0100

    demo

--- src/main/java/opencart/frontend/SuccessPage.java
+++ src/main/java/opencart/frontend/SuccessPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SuccessPage extends BaseClass {
-    @FindBy(xpath = "//h1[text()='Your Account Has Been Created!']")
+    @FindBy(css = "div #content p")
     WebElement accountCreatedSuccessMessage;
     public SuccessPage() {
         PageFactory.initElements(driver,this);
