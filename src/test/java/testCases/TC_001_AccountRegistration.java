c1b7c60f262c4acccea0e8bdbc224f996d775c4d
Your Name <you@example.com>
2022-05-08 00:05:21 +0200

    demo

---
+++ src/test/java/testCases/TC_001_AccountRegistration.java
@@ -0,0 +1,20 @@
+package testCases;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+
+public class TC_001_AccountRegistration {
+    public WebDriver driver;
+
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+}
